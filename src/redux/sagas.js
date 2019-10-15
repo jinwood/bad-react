ccb2cf2777f3ca71fb410f0a7dd43c96cd6be557
jinwood <jinw@protonmail.com>
2019-10-15 14:56:39 +0100

    Remove action

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,6 +1,6 @@
 import { call, takeEvery, put, delay } from 'redux-saga/effects';
 import getTodos from '../api';
-import { FETCH_DATA, FETCH_DATA_ERROR, FETCH_DATA_SUCCESS } from './actions';
+import { FETCH_DATA, FETCH_DATA_SUCCESS } from './actions';
 
 function* getTestData() {
   yield delay(1000); //ignore this
