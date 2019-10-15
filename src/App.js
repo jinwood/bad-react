e3f23349ddddbf80408a19259ab7919e5af0561a
jinwood <jinw@protonmail.com>
2019-10-15 10:16:02 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Main from './components/main';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Main />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const main = () => {
+  return (
+    <div>
+      <h1>Test app</h1>
+      <p>Click the button</p>
+      <input type="button" value="Click me" />
+    </div>
+  );
+};
+
+export default main;
