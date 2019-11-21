cc965866dfbb5c697e3a18fdd832e164f8a13be1
jinwood <jinw@protonmail.com>
2019-11-21 10:37:59 +0000

    Change import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Main from './components/TodoList';
-import './App.css';
+import React from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Main />
+        <TodoList />
       </header>
     </div>
   );
